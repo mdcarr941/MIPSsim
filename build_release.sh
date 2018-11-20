69ef93fe712ceb98ceed60e055ff5cf30171b006
Matthew Carr <mdcarr941@gmail.com>
2018-11-19 23:39:17 -0500

    Updated the build_release script.

--- build_release.sh
+++ build_release.sh
@@ -2,14 +2,14 @@
 # The submitted file should not be a java package, so we remove this from the source
 # file and rename it with a .txt extension.
 
-sed -e 's/package cda5155.proj1;//' src/main/java/cda5155/proj1/MIPSsim.java > MIPSsim.java.txt
+sed -e 's/package cda5155.MIPSsim;//' src/main/java/cda5155/MIPSsim/MIPSsim.java > MIPSsim.java.txt
 cp MIPSsim.java.txt build/MIPSsim.java
-cp sample.txt build/input.txt
+cp proj2/sample.txt build/input.txt
 cd build
 javac MIPSsim.java
 java MIPSsim input.txt
-if test -z $(diff -w -B disassembly.txt ../sample_disassembly.txt)
-  test -z $(diff -w -B simulation.txt ../sample_simulation.txt); then
+if test -z "$(diff -w -B disassembly.txt ../proj2/sample_disassembly.txt)"
+  test -z "$(diff -w -B simulation.txt ../proj2/sample_simulation.txt)"; then
   echo "Success"
 else
   echo "Failure"
